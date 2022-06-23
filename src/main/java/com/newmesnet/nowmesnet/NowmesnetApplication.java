683e160174aaca181e837a7ca600beb7f2887e7f
20joe828698gmailcom19 <joe828698@gmail.com>
2022-06-23 11:34:05 +0800

    创建完成项目，并且能正常运行

---
+++ src/main/java/com/newmesnet/nowmesnet/NowmesnetApplication.java
@@ -0,0 +1,13 @@
+package com.newmesnet.nowmesnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowmesnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NowmesnetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newmesnet/nowmesnet/NowmesnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newmesnet.nowmesnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowmesnetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
